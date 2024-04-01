1ccb07aee9c00a41bc5b9c5ccca98a4a13e15911
usmaniqbal8625 <137871476+usmaniqbal8625@users.noreply.github.com>
2024-04-01 14:35:17 +0500

    ok

--- src/Components/Enrollment.js
+++ src/Components/Enrollment.js
@@ -30,79 +30,81 @@ setShowQuiz(true);
 
   return (
     <>
-    
-        <div className="container bg-light">
-          <div className="row">
-            <div className="col">
-              <h1 className>{topicD?.collectionName}</h1>
-              <div className="d-flex">
-                <h3 className="text-start mx-4">{topicD?.topic}</h3>
-              </div>
-              <p className="mx-md-5 text-start">{topicD?.description.slice(0, 30)}</p>
-
+      <div className="container bg-light">
+        <div className="row">
+          <div className="col">
+            <h1 className>{topicD?.collectionName}</h1>
+            <div className="d-flex">
+              <h3 className="text-start mx-4">{topicD?.topic}</h3>
             </div>
+            <p className="mx-md-5 text-start">
+              {topicD?.description.slice(0, 30)}
+            </p>
           </div>
-          <div className="video-container mt-5 mb-5 d-flex justify-content-center align-items-center">
-  <ReactPlayer
-    url={topicD?.youtubeLink}
-    controls= {false}
-    width="100%"
-    height="100%"
-    
-  />
-</div>
+        </div>
+        <div className="video-container mt-5 mb-5 d-flex justify-content-center align-items-center">
+          <ReactPlayer
+            url={topicD?.youtubeLink}
+            controls={true}
+            width="100%"
+            height="100%"
+            config={{
+              attributes: {
+                controlsList: "nodownload",
+              },
+            }}
+          />
+        </div>
 
-
-          <div className="col-12 col-md-8 mx-auto">
-            <div className="row">
-              <div className="col-md-10">
-                <div className="accordion faq-accordian" id="faqAccordion">
-                  <div
-                    className="card border-0 wow fadeInUp"
-                    data-wow-delay="0.2s"
-                    style={{
-                      visibility: "visible",
-                      animationDelay: "0.2s",
-                      animationName: "fadeInUp",
-                    }}
-                  >
-                    <div className="card-header" id="headingOne">
-                      <h6
-                        className="mb-0 collapsed"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapseOne"
-                        aria-expanded="true"
-                        aria-controls="collapseOne"
-                      >
-                        Summary
-                        <span className="lni-chevron-up"></span>
-                        <i className="fa-solid fa-caret-down"></i>
-                      </h6>
-                    </div>
-                    <div
-                      className="collapse"
-                      id="collapseOne"
-                      aria-labelledby="headingOne"
-                      data-parent="#faqAccordion"
+        <div className="col-12 col-md-8 mx-auto">
+          <div className="row">
+            <div className="col-md-10">
+              <div className="accordion faq-accordian" id="faqAccordion">
+                <div
+                  className="card border-0 wow fadeInUp"
+                  data-wow-delay="0.2s"
+                  style={{
+                    visibility: "visible",
+                    animationDelay: "0.2s",
+                    animationName: "fadeInUp",
+                  }}
+                >
+                  <div className="card-header" id="headingOne">
+                    <h6
+                      className="mb-0 collapsed"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
                     >
-                      <div className="card-body">{topicD?.description}</div>
-                    </div>
+                      Summary
+                      <span className="lni-chevron-up"></span>
+                      <i className="fa-solid fa-caret-down"></i>
+                    </h6>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="collapseOne"
+                    aria-labelledby="headingOne"
+                    data-parent="#faqAccordion"
+                  >
+                    <div className="card-body">{topicD?.description}</div>
                   </div>
                 </div>
               </div>
-              <div className="col-md-2 d-flex align-items-center justify-content-end">
-                <Link
-                  to="/Quiz"
-                  className="px-4 py-2 btn btn-dark text- text-end justify-content-end"
-                  onClick={handleQuizStart}
-                >
-                  Quiz start
-                </Link>
-              </div>
+            </div>
+            <div className="col-md-2 d-flex align-items-center justify-content-end">
+              <Link
+                to="/Quiz"
+                className="px-4 py-2 btn btn-dark text- text-end justify-content-end"
+                onClick={handleQuizStart}
+              >
+                Quiz start
+              </Link>
             </div>
           </div>
         </div>
-     
+      </div>
     </>
   );
 };
