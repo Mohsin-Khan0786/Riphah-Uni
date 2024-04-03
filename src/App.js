fb861c1cae403812c7af8e705cacdcba478de568
usmaniqbal8625 <137871476+usmaniqbal8625@users.noreply.github.com>
2024-04-03 12:09:45 +0500

    offline done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import {  Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar';
-import SignUp from './Pages/SignUp';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Dashboard  from './Pages/Dashboard';
-import Summary from './Pages/Summary';
-import Quiz from './Pages/Quiz';
-import Topics from './Pages/Topics';
-import ProtectedRoute from './ProtectedRoute';
-import Contact from './Pages/Contact';
-import About from './Pages/About'
-import Footer from './Components/Footer';
-
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import SignUp from "./Pages/SignUp";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import Summary from "./Pages/Summary";
+import Quiz from "./Pages/Quiz";
+import Topics from "./Pages/Topics";
+import ProtectedRoute from "./ProtectedRoute";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Footer from "./Components/Footer";
+import Offline from "./Components/Offline";
 
 function App() {
-  return (
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    // Update network status
+    const handleStatusChange = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    window.addEventListener("online", handleStatusChange);
+    window.addEventListener("offline", handleStatusChange);
+
+    return () => {
+      window.removeEventListener("online", handleStatusChange);
+      window.removeEventListener("offline", handleStatusChange);
+    };
+  }, []);
+
+  return isOnline ? (
     <div className="App">
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="About" element={<About />} />
+        <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
-
-        <Route path="/DashBoard" element={<Dashboard />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route
           path="/Summary"
           element={
@@ -50,14 +66,13 @@ function App() {
           }
         />
         <Route path="/Login" element={<Login />} />
-
         <Route path="/Sign-up" element={<SignUp />} />
       </Routes>
-
       <Footer />
     </div>
+  ) : (
+    <Offline/>
   );
 }
 
 export default App;
-
